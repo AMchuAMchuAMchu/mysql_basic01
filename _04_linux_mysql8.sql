169fb34f827a5fd4e386d2f06bd7b00fd6343242
AMchuAMchuAMchu <2466130840@qq.com>
2022-10-05 18:55:30 +0800

    AM 01st commit

--- _04_linux_mysql8.sql
+++ _04_linux_mysql8.sql
@@ -1,13 +1,24 @@
+#
+# show databases ;
+#
+# create database if not exists anime_test01;
+#
+# use anime_test01;
+#
+# show tables ;
+#
+#
+#
+# select *
+# from anime_info;
+#
+#
+#
+#
+#
+#
 
-show databases ;
-
-create database if not exists anime_test01;
-
-use anime_test01;
-
-show tables ;
 
 
 
-select *
-from anime_info;
+
